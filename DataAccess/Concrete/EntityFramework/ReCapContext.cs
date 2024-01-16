cfb9552986eb1c0a2e9da7e7d1d831fe56f20cde
Özge Kestane <ozgekestane97@gmail.com>
2024-01-16 04:02:30 +0300

    IEntityRepository added.

---
+++ DataAccess/Concrete/EntityFramework/ReCapContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ReCapContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=OZGEKESTANE;Database=ReCapProject;Integrated Security=true;");
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+
+
+
+    }
+}
---
+++ dbo.brand.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[BrandId] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(20) NOT NULL
+)
